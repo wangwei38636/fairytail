095d99208c3bdcb39d8ea5c6580553d56d408bca
wangwei <wangwei38636@163.com>
2020-09-19 16:03:24 +0800

    mielong's

---
+++ robots/src/main/java/org/fairytail/robots/RobotsApplication.java
@@ -0,0 +1,13 @@
+package org.fairytail.robots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RobotsApplication.class, args);
+    }
+
+}
---
+++ robots/src/test/java/org/fairytail/robots/RobotsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fairytail.robots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobotsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
