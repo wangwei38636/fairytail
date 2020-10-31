5cbf78d8243be5a4173e57146aa32a2f84707361
wangwei <wangwei38636@163.com>
2020-10-31 11:53:36 +0800

    mielong's

--- jservice/src/main/java/org/fairytail/jservice/bean/config/BeanConfig.java
+++ jservice/src/main/java/org/fairytail/jservice/bean/config/BeanConfig.java
@@ -1,7 +1,6 @@
 package org.fairytail.jservice.bean.config;
 
 import com.alibaba.cloud.sentinel.annotation.SentinelRestTemplate;
-import com.alibaba.nacos.common.utils.ExceptionUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -9,7 +8,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class BeanConfig {
     @Bean
-    @SentinelRestTemplate(blockHandler = "handleException", blockHandlerClass = ExceptionUtil.class)
+    @SentinelRestTemplate
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
